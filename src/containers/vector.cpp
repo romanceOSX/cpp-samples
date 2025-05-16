860291b8f6848e8f2db6b71cf38c575feb0a490c
romance <106858958+romanceOSX@users.noreply.github.com>
2025-05-15 19:44:08 -0600

    feat: vector subproject

--- src/containers/vector.cpp
+++ src/containers/vector.cpp
@@ -1,9 +1,26 @@
 #include <vector>
 #include <iostream>
+#include <map>
 
-int main (int argc, char *argv[]) {
+using StringMap = std::map<std::string, std::string>;
+
+void _map_test() {
+    StringMap sm{{"Hello", "world"}};
+
+    sm.insert({"a", "b"});
+
+    for (auto& [k, v]: sm) {
+        std::cout << k << " : " << v << std::endl;
+    }
+}
+
+void _vector_test() {
     std::vector<int> vi = {1, 2, 3, 4, 5};   
     std::cout << vi.at(0) << std::endl;
+}
+
+int main (int argc, char *argv[]) {
+    _map_test();
     return 0;
 }
 
