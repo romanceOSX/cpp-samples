17bd8500f4dffe4b1ca0f106dbfa772dce85c9e6
romance <106858958+romanceOSX@users.noreply.github.com>
2025-05-26 23:10:40 -0600

    feat: created strings project

--- src/strings/main.cpp
+++ src/strings/main.cpp
@@ -1,8 +1,18 @@
+#include <fstream>
 #include <string>
 #include <iostream>
 
 std::char_traits<char> ct;
 std::string s;
+
+void _test_parse() {
+    std::string filename{"sample.txt"};
+    std::ifstream f{filename, std::ios::binary};
+
+    std::string in;
+    std::getline(f, in);   
+    //std::cout << f << std::endl;
+}
 
 int main (int argc, char *argv[]) {
     
