014adcd9900be13c65b2b6ed6fb0559ff8e7d6f4
romance <106858958+romanceOSX@users.noreply.github.com>
2025-06-07 21:56:18 -0600

    chore: doing atomics

--- src/atomics/main.cpp
+++ src/atomics/main.cpp
@@ -1,13 +1,58 @@
-
 #include <atomic>
+#include <vector>
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+/* barebones spinlock implementation */
+class SpinLockMutex {
+public:
+    SpinLockMutex() = default;
+    void lock() {
+        /* if the value is true, then keep looping since some thread has marked it as busy */
+        while(m_af.test_and_set());
+    }
+    void unlock() {
+        m_af.clear();
+    }
+private:
+    std::atomic_flag m_af = ATOMIC_FLAG_INIT;
+};
 
 /*
  * operations on std::atomic_flag
  */
 
-std::atomic_flag a = ATOMIC_FLAG_INIT;
-int main(int argc, char* argv[]) {
+static SpinLockMutex m;
+
+void waiting_thread() {
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::cout << "Waiting for resource to be available..." << std::endl;
+    m.lock();
+    std::cout << "finally got the resource!" << std::endl;
+}
+
+void actor_thread() {
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::cout << "Locking resource..." << std::endl;
+    m.lock();
+    std::this_thread::sleep_for(std::chrono::seconds(15));
+    std::cout << "Releasing resource!" << std::endl;
+    m.unlock();
+}
+
+void _test_spin_lock_mutex() {
+    std::vector<std::thread> threads;
+    threads.emplace_back(actor_thread);
+    threads.emplace_back(waiting_thread);
+
+    for (auto& thread: threads) {
+        thread.join();
+    }
+}
+
+void _test_atomic_flag() {
+    std::atomic_flag a = ATOMIC_FLAG_INIT;
     /* the first time it results in false because it is empty initialized */
     auto f1 = a.test_and_set();
     /* the second time it returns a 1 because someone else aleady initialized it */
@@ -17,5 +62,10 @@ int main(int argc, char* argv[]) {
     std::cout << f1 << std::endl;
     std::cout << f2 << std::endl;
     std::cout << f3 << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    //_test_atomic_flag();
+    _test_spin_lock_mutex();
 }
 
