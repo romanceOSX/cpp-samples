a80f30b6d5a4eb24da7d34d376a92ae011604a2b
romance <106858958+romanceOSX@users.noreply.github.com>
2025-08-19 21:36:29 -0600

    chore: moving ranges

--- src/ranges/main.cpp
+++ src/ranges/main.cpp
@@ -17,6 +17,9 @@ void print_collection(T&& c) {
 namespace ranges = std::ranges;
 
 void test_ranges() {
+    /*
+     * ranges
+     */
     std::vector v{10, 11, 2, 2, 3, 1, 4, 5};
     print_collection(v);
 
@@ -26,6 +29,28 @@ void test_ranges() {
 
     ranges::sort(ranges::views::reverse(v));
     print_collection(v);
+
+    /* 
+     * adaptors / views
+     */
+    std::vector v2{10, 23, 2, 1, 45, 5, 5};
+    /* drop the first 3 then reverse the thing */
+    auto v_adaptor = v2 | std::views::reverse | std::views::drop(3);
+    std::cout << *v_adaptor.begin() << std::endl;
+    print_collection(v_adaptor);
+    print_collection(v2);
+
+    /*
+     *
+     */
+    std::vector c{1, 2, 4, 5, 2, 1, 10, 38, 2};
+    print_collection(c);
+    auto res = std::move(c)
+        | std::views::drop(3)
+        | std::views::reverse
+        | std::views::filter([](auto& e) { return e < 10; })
+        | std::ranges::to<std::vector>();
+    print_collection(res);
 }
 
 int main (int argc, char *argv[]) {
