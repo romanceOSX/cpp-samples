5446f2b7645d720a88654045cbec9dfbf103554d
romance <106858958+romanceOSX@users.noreply.github.com>
2025-09-06 00:13:41 -0600

    chore: updating

--- src/exceptions/main.cpp
+++ src/exceptions/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <stdexcept>
+
+#include <cerrno>
+
+void _errno_test() {
+    EDOM;
+}
 
 int main (int argc, char *argv[]) {
     try {
-        throw 3;
+        //throw 3;
     }
     catch (std::exception& e) {
         std::cout << e.what() << std::endl;
@@ -12,6 +19,9 @@ int main (int argc, char *argv[]) {
             << "I really don't know what failed..."
             << std::endl;
     }
+
+    throw std::runtime_error("SOMETHING BAD HAPPENED");
     
     return 0;
 }
+
