368459edd18cbf50e0a77627758bc243b5a316c3
romance <106858958+romanceOSX@users.noreply.github.com>
2025-10-03 22:57:11 -0600

    chore: adding a c++20 example

--- src/strings/input_file.cpp
+++ src/strings/input_file.cpp
@@ -3,6 +3,11 @@
 #include <stdexcept>
 #include <string>
 
+// C++20
+#include <filesystem>
+namespace fs = std::filesystem;
+
+// non C++20
 std::string read_file(const std::string& path) {
     std::ifstream in(path, std::ios::binary);
     if (!in)
@@ -14,9 +19,22 @@ std::string read_file(const std::string& path) {
     );
 }
 
+// C++ 20
+std::string readFileCpp20(const std::string& path) {
+    std::ifstream in(path, std::ios::binary);
+    if (!in)
+        std::runtime_error("Failed to open file!");
+
+    std::string buffer(fs::file_size(path), '\0');
+    in.read(buffer.data(), buffer.size());
+
+    return buffer;
+}
+
 int main (int argc, char *argv[]) {
     std::cout << "doing input file" << std::endl;
-    std::string f = read_file("main.cpp");
+    //std::string f = read_file("main.cpp");
+    std::string f = readFileCpp20("main.cpp");
     std::cout << f << std::endl;
     return 0;
 }
